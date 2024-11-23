b5166c0f2d1460e22f1b1815c7896103ce1a3de6
EluciusFTW <guysbuss@gmail.com>
2024-11-23 16:46:48 +0100

    Used Commandline and separated existing logic into three commands.

--- Console/Program.fs
+++ Console/Program.fs
@@ -1,18 +1,62 @@
-﻿open Console
+﻿module Todoist.Console
+
+open Console
+open FSharp.SystemCommandLine
 open Spectre.Console
 open SpectreCoff
 open Tasks
 
-let choices = ["Add Task"; "Manipulate Today"]
+module Commands = 
+    let AddTodo = 
+        command "addTodo" {
+            description "Adds a Todo-item"
+            setHandler addTask
+        }
 
-LocalState.init ()
+    let UpdateTodo = 
+        command "updateTodo" {
+            description "Updates a Todo-item"
+            setHandler manipulateToday
+        }
 
-while true do
-    AnsiConsole.Clear |> ignore
-    let choice = chooseFrom choices "What do you want to do?"
+    let Interactive = 
+        let repeat = Input.Option<bool>("--repeat", false)
 
-    match choice with
-    | "Add Task" -> addTask ()
-    | "Manipulate Today" -> manipulateToday ()
-    | _ -> failwith "Choice does not exist"
+        let doWhile f condition =
+            f ()
+            while condition do f ()
 
+        let handlerLogic () = 
+            AnsiConsole.Clear |> ignore
+            let choice = chooseFrom ["Add Task"; "Manipulate Today"] "What do you want to do?"
+            match choice with
+            | "Add Task" -> addTask ()
+            | "Manipulate Today" -> manipulateToday ()
+            | _ -> failwith "Choice does not exist"
+
+        let handler ctx = 
+            doWhile handlerLogic (repeat.GetValue ctx)
+            ()
+
+        command "interactive" {
+            description "Walks you through the choices"
+            inputs (Input.Context())
+            setHandler handler
+        }
+    
+    let Root argv = 
+        let noSubcommnadHandler () = 
+            C "Please specify a command!" |> toConsole
+
+        rootCommand argv {
+            description "Todoist root"
+            setHandler noSubcommnadHandler
+            addCommand Interactive
+            addCommand AddTodo
+            addCommand UpdateTodo
+        }
+
+[<EntryPoint>]
+let main argv = 
+    LocalState.init ()
+    Commands.Root argv    
